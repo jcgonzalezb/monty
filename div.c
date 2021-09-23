7cb8af06d24f3a33c35a3740faf9c288606b4101
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-22 21:39:17 -0500

    operations corrections

--- div.c
+++ div.c
@@ -9,12 +9,13 @@ void _div(stack_t **h, unsigned int line_number)
 {
 	if (*h == NULL || (*h)->next == NULL)
 	{
-		printf("L%u: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*h)->n == 0 || (*h)->next->n == 0)
 	{
-		printf("L%u: division by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	(*h)->next->n /= (*h)->n;
 	(*h) = (*h)->next;
--- mul.c
+++ mul.c
@@ -9,7 +9,7 @@ void _mul(stack_t **h, unsigned int line_number)
 {
 	if (*h == NULL || (*h)->next == NULL)
 	{
-		printf("L%u: can't mul, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*h)->next->n *= (*h)->n;
